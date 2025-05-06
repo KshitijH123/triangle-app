e5baf9242bbb88cb17a1fad9ebec2af1e2d41d57
KshitijH123 <kshitijhapase@gmail.com>
2025-05-06 18:46:39 +0530

    triangle area calculator

---
+++ app/model/Triangle.ts
@@ -0,0 +1,11 @@
+export class Triangle {
+  height: number;
+  width: number;
+  area: number;
+
+  constructor(height: number, width: number, area: number) {
+    this.height = height;
+    this.width = width;
+    this.area = area;
+  }
+}
